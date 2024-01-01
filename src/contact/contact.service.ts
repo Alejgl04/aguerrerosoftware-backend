d24c383905fac547bd09812cc16e46faae1c0a09
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-12-31 19:58:04 -0500

    CONTACT SERVICES

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { CreateContactDto } from './dto/create-contact.dto';
 
@@ -6,27 +6,40 @@ import { CreateContactDto } from './dto/create-contact.dto';
 export class ContactService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async create(createContactDto: CreateContactDto) {
-    await this.mailerService
-      .sendMail({
+  create(createContactDto: CreateContactDto) {
+    const { name, email, services, message } = createContactDto;
+    return new Promise((resolve, reject) => {
+      const sendMailAdmin = this.mailerService.sendMail({
         to: 'alejo.jesus.magne@gmail.com', // list of receivers
-        from: 'noreply@nestjs.com', // sender address
-        subject: 'Testing Nest MailerModule ✔', // Subject line
-        text: 'welcome', // plaintext body
+        from: 'admin@info.com', // sender address
+        subject: 'New Message From: aguerrerosoftware.com ✔', // Subject line
         html: `
-          <h2>Welcome</h2>
-          ${createContactDto.services} <br />
-          ${createContactDto.email} <br />
-          ${createContactDto.name} <br />
-          ${createContactDto.message}
+        <div style="background:#cdcdcd;padding:20px;font-size:20px;">
+          <div style="background:white;width: 70%;margin: auto;padding: 15px;">
+            <img src="cid:logo"/>
+            <hr>
+            Hello Alejandro.
+            <br><br>
+            ${name} has sent a new message. 
+            <br><br>
+            Here is the body content:
+            <br>
+            <b>Name:</b> ${name} <br>
+            <b>Email:</b> ${email} <br>
+            <b>Services:</b> - ${services} - <br>
+            <b>Message:</b> ${message}
+            </div>
+        </div>
         `, // HTML body content
-      })
-      .then(() => {
-        console.log('true');
-      })
-      .catch(() => {
-        console.log('false');
       });
+      if (sendMailAdmin) {
+        resolve({
+          ok: true,
+          message: 'Message has been sent successfully',
+        });
+      }
+      return reject(false);
+    });
   }
 
   findAll() {
